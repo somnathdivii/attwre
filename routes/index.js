9513fdc42d500eabae5b54d31d79ef3677eaaed8
Somnath <61135875+somnathdivii@users.noreply.github.com>
2021-08-21 13:47:39 -0700

    admin design updated.

--- routes/index.js
+++ routes/index.js
@@ -15,12 +15,53 @@ var pageTitle = '';
 
 
 router.get('/', function (req, res, next) {
+	if (req.session.userId) {
+		res.redirect('/dashboard');
+		// if (req.session.access === 'admin') {
+
+		// }else{
+
+		// }
+	} else {
+
+		pageTitle = 'Login';
+		return res.render('login.ejs', { 'title': pageTitle });
+	}
+
+});
+
+router.post('/', function (req, res, next) {
+	//console.log(req.body);
+	User.findOne({ email: req.body.email }, function (err, data) {
+		if (data) {
+
+			if (data.password == req.body.password) {
+				//console.log("Done Login");
+				req.session.userId = data.unique_id;
+				req.session.access = data.access;
+
+				//console.log(req.session.userId);
+				res.redirect('/dashboard');
+				// res.send({ "Success": "Success!" });
+
+			} else {
+				res.send({ "Success": "Wrong password!" });
+			}
+		} else {
+			res.send({ "Success": "This Email Is not regestered!" });
+		}
+	});
+});
+
+
+
+router.get('/registration', function (req, res, next) {
 	pageTitle = 'Employee Registration';
 	return res.render('index.ejs', { 'title': pageTitle });
 	// return res.render('index', {page:'Home', menuId:'home'});
 });
 
-router.post('/', function (req, res, next) {
+router.post('/registration', function (req, res, next) {
 	console.log(req.body);
 	var personInfo = req.body;
 
@@ -62,14 +103,12 @@ router.post('/', function (req, res, next) {
 								});
 
 							}).sort({ _id: -1 }).limit(1);
+							// res.redirect('/');
 							res.send({ "Success": "You are regestered,You can login now." });
-
 						} else {
 							res.send({ "Success": "Username is already used." });
 						}
 					});
-
-
 
 				} else {
 					res.send({ "Success": "Email is already used." });
@@ -82,35 +121,10 @@ router.post('/', function (req, res, next) {
 	}
 });
 
-router.get('/login', function (req, res, next) {
-	pageTitle = 'Login';
-	return res.render('login.ejs', { 'title': pageTitle });
-});
 
-router.post('/login', function (req, res, next) {
-	//console.log(req.body);
-	User.findOne({ email: req.body.email }, function (err, data) {
-		if (data) {
 
-			if (data.password == req.body.password) {
-				//console.log("Done Login");
-				req.session.userId = data.unique_id;
-				req.session.access = data.access;
-
-				//console.log(req.session.userId);
-				res.send({ "Success": "Success!" });
-
-			} else {
-				res.send({ "Success": "Wrong password!" });
-			}
-		} else {
-			res.send({ "Success": "This Email Is not regestered!" });
-		}
-	});
-});
-
-router.get('/profile', function (req, res, next) {
-	console.log("profile");
+router.get('/dashboard', function (req, res, next) {
+	console.log("dashboard");
 	User.findOne({ unique_id: req.session.userId }, function (err, data) {
 		console.log("data");
 		console.log(data);
@@ -194,7 +208,7 @@ router.post('/forgetpass', function (req, res, next) {
 // 			}else{
 // 				res.redirect('/profile');
 // 			}
-			
+
 // 		}
 // 	});
 // });
